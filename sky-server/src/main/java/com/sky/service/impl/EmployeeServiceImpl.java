fc3896cf6e404c0dba27a9b377d2b7319ac4d355
张凯祥 <2250753108@qq.com>
2024-04-30 10:06:36 +0800

    20240430

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -62,8 +62,8 @@ public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> i
 
         //密码比对
         // 前端传过来的铭文需要进行md5加密，然后再进行比对
-
         password = DigestUtils.md5DigestAsHex(password.getBytes());
+        log.info(password);
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
